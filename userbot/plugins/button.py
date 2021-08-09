3ace07686de9b59252a9fed544d7eb4af442ea3d
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-08-09 16:19:55 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -1,5 +1,6 @@
-#    Copyright (C) 2020  sandeep.n(π.$)
-# button post makker for catuserbot thanks to uniborg for the base
+#    Copyright (C) 2020  s
+# button post makker for catuserbot 
+
 
 # by @sandy1709 (@mrconfused)
 import os
@@ -10,7 +11,7 @@ from telethon import Button
 from ..Config import Config
 from . import catub, edit_delete, reply_id
 
-plugin_category = "tools"
+plugin_category = "plugins"
 # regex obtained from:
 # https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
